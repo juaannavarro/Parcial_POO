af4a5de01f2893dfbeed3d967ec88dba26dcd614
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-24 08:13:35 +0000

    Hemos creado una función y hemos implementado un menú para saber todo sobre cada animal

--- ejercicios/animal.py
+++ ejercicios/animal.py
@@ -1,3 +1,19 @@
 class animal:
     def __init__(self, animal):
         self.animal=animal
+
+    def Animal(pollo, gato, ornitorrinco):
+        print("Seleccione: ")
+        print("1. Pollo")
+        print("2. Gato")
+        print("3. Ornitorrinco")
+        print()
+        eleccion = int(input("Cuál es tu elección?: "))
+
+        if eleccion == 1:
+            print("Es un animal ovíparo")
+        elif eleccion == 2:
+            print("Es un animal mamífero")
+        elif eleccion == 3:
+            print("Es un animal ovíparo y mamífero")
+print(animal.Animal("pollo", "gato", "ornitorrinco"))
