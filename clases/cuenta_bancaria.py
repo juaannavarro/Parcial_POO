86ade723f13fb56c6e4970ca50ad269f5c519fa3
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2022-03-24 09:10:28 +0000

    arreglados los errores y creada la segunda cuenta bancaria

--- clases/cuenta_bancaria.py
+++ clases/cuenta_bancaria.py
@@ -8,10 +8,10 @@ class cuenta_bancaria:
         self.nombre=nombre
         self.fechaA=fechaA
         self.saldo=saldo
-        self.numero=numero
-
-    def cuenta_Bancaria(retirar,ingresar,transferir):
-        print("Seleccione: ")
+        self.numero=numero  
+       
+    def cuenta_Bancaria1(retirar,ingresar,transferir):
+        print("Cuenta bancaria 1. Seleccione: ")
         print(" 1.retirar")
         print(" 2.transferir")
         print(" 3.ingresar")
@@ -19,30 +19,54 @@ class cuenta_bancaria:
         eleccion = int(input("Cuál es tu elección?: "))
 
         if eleccion == 1:
-            print(retirar)
+            retirar =int(input("¿Cuánto dinero quieres retirar?:"))
+            while retirar != num_movimientos_max:
+                if retirar > saldo:
+                    print("No puedes retirar dinero")
+                else:
+                    print("Ha retirado su dinero correctamente, tu saldo actual es: ", )
+                    print(saldo-retirar)
+                break  
         elif eleccion == 2:
-            print(transferir)
+            transferir =int(input("¿Cuánto dinero quieres transferir?:"))
+            while transferir != num_movimientos_max:
+                if transferir > saldo:
+                    print("No puedes transferir dinero")
+                else:
+                    print("Ha transferido su dinero correctamente")
+                break
         elif eleccion == 3:
-            print(ingresar)
-        
-    def Retirar(retirar): 
-        retirar =int(input("¿Cuánto dinero quieres retirar?:"))
-        while retirar != num_movimientos_max:
-            if retirar > saldo:
-                print("No puedes retirar dinero")
-            else:
-                print("Ha retirado su dinero correctamente, tu saldo actual es: ", )
-                print(saldo-retirar)
-            break      
-        
-    def Transferir(transferir):  
-        transferir =int(input("¿Cuánto dinero quieres transferir?:"))
-        while transferir != num_movimientos_max:
-            if transferir > saldo:
-                print("No puedes transferir dinero")
-            else:
-                print("Ha transferido su dinero correctamente")
-            break
-print(cuenta_bancaria.cuenta_Bancaria("retirar","transferir", "ingresar"))
-print(cuenta_bancaria.Retirar("retirar"))
-print(cuenta_bancaria.Transferir("transferir"))
+            ingresar =int(input("¿Cuánto dinero quieres ingresar?:"))
+            print("Tu saldo actual es: ")
+            print(saldo+ingresar)
+    def cuenta_Bancaria2(retirar,ingresar,transferir):
+        print("Cuenta bancaria 2. Seleccione: ")
+        print(" 1.retirar")
+        print(" 2.transferir")
+        print(" 3.ingresar")
+        print()
+        eleccion = int(input("Cuál es tu elección?: "))
+
+        if eleccion == 1:
+            retirar =int(input("¿Cuánto dinero quieres retirar?:"))
+            while retirar != num_movimientos_max:
+                if retirar > saldo:
+                    print("No puedes retirar dinero")
+                else:
+                    print("Ha retirado su dinero correctamente, tu saldo actual es: ", )
+                    print(saldo-retirar)
+                break  
+        elif eleccion == 2:
+            transferir =int(input("¿Cuánto dinero quieres transferir?:"))
+            while transferir != num_movimientos_max:
+                if transferir > saldo:
+                    print("No puedes transferir dinero")
+                else:
+                    print("Ha transferido su dinero correctamente")
+                break
+        elif eleccion == 3:
+            ingresar =int(input("¿Cuánto dinero quieres ingresar?:"))
+            print("Tu saldo actual es: ")
+            print(saldo+ingresar)
+print(cuenta_bancaria.cuenta_Bancaria1("retirar","transferir", "ingresar"))
+print(cuenta_bancaria.cuenta_Bancaria2("retirar","transferir", "ingresar"))
